4635163fc16202a6e99ef3438e3972a1b44e7375
mooz <stillpedant@gmail.com>
2009-11-14 13:49:56 +0900

    Added status bar icon to the prefer ldrize
    - ごめんなさい

--- plugins/prefer-ldrize.ks.js
+++ plugins/prefer-ldrize.ks.js
@@ -3,9 +3,9 @@
 var PLUGIN_INFO =
 <KeySnailPlugin>
     <name>Prefer LDRize</name>
-    <description>I prefer LDRize</description>
+    <description>Prefer LDRize keyboard shortcut</description>
     <description lang="ja">LDRize と KeySnail で快適ブラウジング</description>
-    <version>1.0.1</version>
+    <version>1.0.2</version>
     <updateURL>http://github.com/mooz/keysnail/raw/master/plugins/prefer-ldrize.ks.js</updateURL>
     <iconURL>http://github.com/mooz/keysnail/raw/master/plugins/icon/prefer-ldrize.icon.png</iconURL>
     <author mail="stillpedant@gmail.com" homepage="http://d.hatena.ne.jp/mooz/">mooz</author>
@@ -157,6 +157,10 @@ LDRize と KeySnail で快適なブラウジングを！
 // }} ======================================================================= //
 
 // ChangeLog {{ ============================================================= //
+// 
+// ==== 1.0.2 (2009 11/14) ====
+// 
+// * Added statusbar icon.
 //
 // ==== 1.0.1 (2009 11/14) ====
 //
@@ -197,43 +201,110 @@ var preferLDRize =
              return key.keyMapHolder[key.modes.LDRIZE] = (aBool) ? ldrizeKeymap : undefined;
          }
 
-         function decideKeyMap() {
-             var keymap;
-
-             keymap = setKeymap(ldrizeEnabled());
+         function setStatusbarIcon() {
+             var keymap = key.keyMapHolder[key.modes.LDRIZE];
 
              // change statusbar icon
-             if (keymap && key.status && !key.suspended)
+             if (keymap)
              {
-                 iconElem.setAttribute("src", iconData);
-                 iconElem.tooltipText = M({en: "LDRize preferred",
-                                           ja: "LDRize が優先されてますぅ"});
+                 if (status)
+                 {
+                     iconElem.setAttribute("src", enabledIconData);                         
+                     iconElem.tooltipText = M({en: "LDRize preferred",
+                                               ja: "LDRize が優先されてますぅ"});
+                 }
+                 else
+                 {
+                     iconElem.setAttribute("src", disabledIconData);
+                     iconElem.tooltipText = M({en: "Prefer LDRize disabled",
+                                               ja: "Prefer LDRize にはちょっとお休みしてもらってます"});
+                 }
              }
              else
              {
-                 if (!key.keyMapHolder[key.modes.SITELOCAL])
-                     key.updateStatusBar();
+                 iconElem.setAttribute("src", disabledIconData);
+                 iconElem.tooltipText = M({en: "LDRize is not enabled",
+                                           ja: "ここは LDRize の管轄外ですよね"});
              }
+         }
+
+         function decideKeyMap() {
+             setKeymap(ldrizeEnabled());
+             setStatusbarIcon();
          }
 
          // }} ======================================================================= //
 
          // Status bar icon {{ ======================================================= //
 
-         var iconElem = document.getElementById("keysnail-statusbar-icon");
-         var iconData = 'data:image/png;base64,' +
-             'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlz' +
-             'AAACVgAAAlYBSEjC2wAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAInSURB' +
-             'VDiNlZBfSJNRGIef8+3zczptNR2M+QedG8TAP2sKZYtMqChhKGRBs6uKFCFEKryMui4MIzCQiCCq' +
-             'i9Cbki6iOci6SDC1oGka6RaYlYON1tq3LhxfrbTauTq87+95+J0jhsbPdsyEAwOzHyc2k8VxmN0R' +
-             'V+m+Xp29RfVnCwN8ioVzE2q8Wc4GrirehtPSSJGhBFVNsrT6pkD+H9Bp8XCy8RK2oro/dv8U7N96' +
-             'gi7PAEJI6+7/KnCV7qVr11UEAoDYtwjjC8NMh8cwKEYayltYXwtIQkenZ0CDJ5cec+xWCQJBR/0F' +
-             '2l19qKnkxg3qyw9gKawE4HPsA+cftDDkm8OUb81ouGGDWmuzdh+Z6qettjcD1pr+Pjhc14ckdBQX' +
-             'lGmz8YUR7GZ3Rm46PEb8e2xNIBCUbXECsBxdRE0lSSS/amGj3szT+eEMgWWTjYevBtcEsk6hxtqU' +
-             'fm8Yq9HO/MqkFvZWnyYwdxf/7B2SaoJo/AvXAt0IISG815UUgMd2iInFRxTmmmhyHOVJ8DaDR14j' +
-             'hESKFBdHW3nxflSTKnI+N33vfgqsRgfbK7wMv+znctszeu43cGrnFQ46O9NIiuDyBDPhAMY8Mzsq' +
-             'W9HLBiSH2R0BCK0G8dja0UkyiqynwlTNjed9TIX8aYHAYXbTWtPDHocPvWwAiOq6zxxfTqjx5pVo' +
-             'SKm27iYvp4B8pRB9joHpkJ/A23sIIVFV7EKWlF//MQqc+wEil6ylCDY4SAAAAABJRU5ErkJggg==';
+         var enabledIconData = 'data:image/png;base64,' + 
+             'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A' +
+             '/wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9kLDgQHD7Ydgu8AAAAZdEVYdENv' +
+             'bW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAACTUlEQVQ4y22Tv0vbbRTFPzdWaCGiiwRXB3Gp' +
+             'oKMQxN3B/gEZ7Ozi4OQcDCSD/gkBq4tTliLZnCLUwSHSZhKimZQgjcSCzzkdvon6vvTAw3Mvz69z' +
+             '7j1PSJJtcrlcANgGEJADTAQGwiYARRAQ2I6IyI0OAxhwRDgicLYB7u/h50+IyEa7TUgmInvNtre3' +
+             'tzW+APDGxoZsO33/LoESWNWq/PWrBBJYv37JtskUOEvAI+jFtj5/tsAJZLCz2ALryxfZdu6d7v8g' +
+             'B/DxI5Gl4YzdGz59ymZJyXayLUAjNkm23G4nQUoZ7eQsToKkP3+S7IRtDwYDdbtdA765ufHj46PG' +
+             'UtRoaEQ70w5St/sm27YqlYpmZ2e1tLTkQqHgnZ2dZFujFit1OhmDqSnp928pW0u2HSPqY4wko3fx' +
+             'qyn+gfhQrVa5u7uj3+/H4eGhZ2ZmYn9/P3q9XgyHQ6+trVEqlTj69o3LHz8YDAZMT09TqVSYmJiA' +
+             'drutubk51ev1V91HR0dpfX1dzWYzraysqFarqd/vJ0AXFxfp4OBA8/Pz6bUGi4uLur+/H0m2z8/P' +
+             'U6lUGhdKk5OTGnfp8vJSe3t7Wl5efvPB2AsRwebmJvl8Hr8zx8vLy9jqnJycUC6XabVa2frV1ZUL' +
+             'hYLq9bqbzaYBNxqNVCwWfXZ2ptXVVe3u7urh4UGAOp2Oer2eAd3e3jpqtZq63S7D4RBJLCwskM/n' +
+             'fX19HU9PTy4Wi7G1tcXx8bFbrVZMTU1RLpc5PT318/NzxPgPvG/b/3NJ4x/7PjcQfwEbKv+abz+B' +
+             'nAAAAABJRU5ErkJggg==';
+
+         var disabledIconData = 'data:image/png;base64,' +
+             'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A' +
+             '/wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9kLDgQIEcuKo0MAAAAZdEVYdENv' +
+             'bW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAACYUlEQVQ4y22SPUszURCFn7OJxCyiGBvRWkQi' +
+             'Bi2iCKKFkPRaiKWgrb3Y2lr4A6z8CWr8QssE0ohoKShCQAhZyIZoDN55i3dX8n5cGO7M3JnLOZwj' +
+             '55wzMzzPE4CZATjAA0xS3CPOJcnMTJK8aBnAooV4ySTR6XQIwxBJAIRh+PMGmMzMTk5O7Pb29vcE' +
+             'kMvlbHd3V+/v71YulwE0PT1tYRjy+voKoNXVVRsYGJCccybJAG1tbXF8fBwxMd3d3dFsNomQ9VJk' +
+             'bGzM8vm8vN5m75FEIpGIocv+Gopo4wEuQvBPzM7OOjNzPT0Xx9zcnAHOk+R1Oh01Gg0Bqtfr+vj4' +
+             'EKDBwUHNz88LUITCA1QoFLxINU9m5s7Pz7m8vGRoaEjNZpOFhQW3sbGhSDJarRY3NzfW19enQqFA' +
+             'MpmMlfj9QS/16HY9eaw9/zlKlkolgiCg3W5rc3PTfN/X2dmZgiBQt9u1yclJFhcXKZfLvLy88Pn5' +
+             'ie/7rK+v43ke3szMDNVqlampKfN9X4BlMhmr1WqWz+ft6uqKi4sLcrmcXV9fs7y8bJlMhr29PZNE' +
+             'cnx8nHQ6TS6X+7HpyMiIDQ8PK5vNks1m2d7eplgsCiCRSCgMQ9LptGIZ/+B5dHREKpWKrQ3A9/f3' +
+             'T12pVDg9PWV/f98AvLe3N7XbbR4eHvT09MT9/T1BEFCv1/X4+MjBwQHFYpFWqyWAlZUVDg8PtbOz' +
+             'oyAIUKlUco1Gg263i3OO0dFRUqmU1Wo1fX192cTEhJaWlqhUKvb8/Kz+/n7W1taoVqvW7XYVW9R6' +
+             'Zfu7ds79WLenNkC/AENaUIUshyj4AAAAAElFTkSuQmCC';
+
+         function setAttributes(aElem, aAttributes) {
+             for (var key in aAttributes) {
+                 aElem.setAttribute(key, aAttributes[key]);
+             }
+         }
+
+         const CONTAINER_ID = "keysnail-prefer-ldrize-container";
+         const ICON_ID      = "keysnail-prefer-ldrize-icon";
+
+         var statusbar      = document.getElementById("status-bar");
+         var statusbarPanel = document.getElementById("keysnail-status");
+         var container      = document.getElementById(CONTAINER_ID);
+         var iconElem       = document.getElementById(ICON_ID);
+
+         if (!container) {
+             // create a new one
+             container = document.createElement("statusbarpanel");
+             setAttributes(container,
+                           {
+                               align: "center",
+                               id: CONTAINER_ID
+                           });
+
+             iconElem = document.createElement("image");
+             setAttributes(iconElem,
+                           {
+                               id: ICON_ID,
+                               src : disabledIconData
+                           });
+
+             container.appendChild(iconElem);
+
+             statusbar.insertBefore(container, statusbarPanel);
+         }
+
+         iconElem.onmouseup = function () { self.toggleStatus(); };
 
          // }} ======================================================================= //
 
@@ -255,7 +326,10 @@ var preferLDRize =
              if (sandbox.window.LDRize != undefined && sandbox.window.Minibuffer != undefined)
              {
                  sandbox.window.addEventListener("focus", function () { decideKeyMap(); }, false);
-                 sandbox.window.addEventListener("blur", function () { setKeymap(false); }, false);
+                 sandbox.window.addEventListener("blur", function () {
+                                                     setKeymap(false);
+                                                     setStatusbarIcon();
+                                                 }, false);
 
                  try
                  {
@@ -319,19 +393,15 @@ var preferLDRize =
 
          var self = {
              toggleStatus: function toggleStatus() {
-                 if (status)
-                 {
-                     // disable
-                     status = false;
-                     setKeymap(false);
-                     key.updateStatusBar();
-                 }
-                 else
-                 {
-                     // enable
-                     status = true;
-                     setKeymap(true);
-                 }
+                 status = !status;
+                 setKeymap(status);
+
+                 setStatusbarIcon();
+
+                 display.echoStatusBar(M({ja: "Prefer LDRize が", en: "Prefer LDRize"}) +
+                                       (status ?
+                                        M({ja: "有効になりました", en: " enabled"}) :
+                                        M({ja: "無効になりました", en: " disabled"})));
 
                  gBrowser.focus();
                  _content.focus();
@@ -353,24 +423,6 @@ if (preferLDRize)
                en: "Toggle prefer ldrize status"}));
 
     // Make LDRize commands as exts  {{ ========================================= //
-
-    // var ldRizeKeys = {
-    //     "M-x" : "Open Minibuffer",
-    //     "?"   : "Toggle help",
-    //     "j"   : "Scroll next item",
-    //     "k"   : "Scroll previous item",
-    //     "p"   : "Pin",
-    //     "l"   : "Toggle pinned items list",
-    //     "f"   : "Focus on search box",
-    //     "v"   : "Open in current tab",
-    //     "o"   : "Open pinned items or current item",
-    //     "i"   : "Open in iframe",
-    //     "s"   : "Change Siteinfo"
-    // };
-
-    // function makeExtName(aDescription) {
-    //     return "prefer-ldrize-" + aDescription.replace(" ", "-", "g").toLowerCase();
-    // }
 
     var ldRizeKeys = {
         ":" : ["prefer-ldrize-open-minibuffer"                  , M({en: "Open Minibuffer",                  ja: "ミニバッファを開く"})],
