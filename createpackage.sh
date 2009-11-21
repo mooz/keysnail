22df4bfcbe89f7875352e1b7c8c1a7189ebf9b53
mooz <stillpedant@gmail.com>
2009-11-21 21:36:53 +0900

    Oops
    - schemes ディレクトリを追加し忘れた。

--- createpackage.sh
+++ createpackage.sh
@@ -5,16 +5,16 @@ setopt extended_glob
 ## ================================ ##
 
 # generate documentaion
-./generate-init-file.py ja doc > ./content/resources/.keysnail.js.doc.ja
-./generate-init-file.py en doc > ./content/resources/.keysnail.js.doc.en
+# ./generate-init-file.py ja doc > ./content/resources/.keysnail.js.doc.ja
+# ./generate-init-file.py en doc > ./content/resources/.keysnail.js.doc.en
 
-# generate emacs key scheme
-./generate-init-file.py ja emacs > ./content/resources/.keysnail.js.emacs.ja
-./generate-init-file.py en emacs > ./content/resources/.keysnail.js.emacs.en
+# # generate emacs key scheme
+# ./generate-init-file.py ja emacs > ./content/resources/.keysnail.js.emacs.ja
+# ./generate-init-file.py en emacs > ./content/resources/.keysnail.js.emacs.en
 
-# generate empty init file
-./generate-init-file.py ja empty > ./content/resources/.keysnail.js.ja
-./generate-init-file.py en empty > ./content/resources/.keysnail.js.en
+# # generate empty init file
+# ./generate-init-file.py ja empty > ./content/resources/.keysnail.js.ja
+# ./generate-init-file.py en empty > ./content/resources/.keysnail.js.en
 
 ## ================================ ##
 
@@ -22,7 +22,7 @@ setopt extended_glob
 rm -f chrome/keysnail.jar
 jar cvf0 chrome/keysnail.jar \
     content/*.{js,xul,xhtml}~(*~|.svn/*) \
-    content/resources/{.*~*~,*~*~} \
+    content/resources/*~*~ \
     content/modules/*.js~(*~|.svn/*) \
     locale/**/*.*~(*~|.svn/*) \
     skin/**/*.*~(*~|*.svg|.svn/*)
@@ -34,6 +34,7 @@ zip -r -9 keysnail.xpi \
     defaults/**/*.*~(*~|.svn/*) \
     install.rdf \
     share/*.js~(*~|.svn/*) \
+    schemes/*.js~(*~|.svn/*) \
     components/*.js~*~
 cp chrome.manifest.pack /tmp/chrome.manifest
 zip -j -9 keysnail.xpi /tmp/chrome.manifest
