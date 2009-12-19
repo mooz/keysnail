d12f6a1272e1e16c06a25e5008cbfd4f83d31eeb
mooz <stillpedant@gmail.com>
2009-12-19 12:46:37 +0900

    added rootContentViewer to prompt module
    - 高速化を試みたが、全く変化は見られず

--- content/modules/prompt.js
+++ content/modules/prompt.js
@@ -13,6 +13,8 @@ KeySnail.Prompt = function () {
     const Cc = Components.classes;
     const Ci = Components.interfaces;
 
+    var rootContentViewer;
+
     var modules;
 
     // defalut key settings
@@ -767,6 +769,8 @@ KeySnail.Prompt = function () {
     }
 
     function updateSelector(aContext) {
+        rootContentViewer.hide();
+
         removeAllChilds(listbox);
 
         if (aContext.compIndexList === null)
@@ -780,12 +784,16 @@ KeySnail.Prompt = function () {
             setListBoxFromIndexList(wholeList, compIndexList);
             setRows(compIndexList.length);
         }
-        listbox.hidden = false;
+
+        if (listbox.hidden)
+            listbox.hidden = false;
 
         selectorDisplayStatusbarLine(currentRegexp, 0, compIndexList ?
                                      compIndexList.length : wholeList.length);
 
         setListBoxSelection(compIndexList ? compIndex : wholeListIndex);
+
+        rootContentViewer.show();
     }
 
     function setListBoxIndex(aIndex) {
@@ -1438,6 +1446,12 @@ KeySnail.Prompt = function () {
                 return;
 
             modules = this.modules;
+
+            rootContentViewer = window.top
+                .QueryInterface(Ci.nsIInterfaceRequestor)
+                .getInterface(Ci.nsIWebNavigation)
+                .QueryInterface(Ci.nsIDocShell)
+                .contentViewer;
 
             promptbox = document.getElementById("keysnail-prompt");
             label     = document.getElementById("keysnail-prompt-label");
