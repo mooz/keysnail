3dc260a880e44ddbbf665d2719ca31e3390af451
mooz <stillpedant@gmail.com>
2010-01-09 00:20:22 +0900

    Now keysnail can be automatically updated

--- createpackage.sh
+++ createpackage.sh
@@ -4,20 +4,6 @@ setopt extended_glob
 
 ## ================================ ##
 
-# generate documentaion
-# ./generate-init-file.py ja doc > ./content/resources/.keysnail.js.doc.ja
-# ./generate-init-file.py en doc > ./content/resources/.keysnail.js.doc.en
-
-# # generate emacs key scheme
-# ./generate-init-file.py ja emacs > ./content/resources/.keysnail.js.emacs.ja
-# ./generate-init-file.py en emacs > ./content/resources/.keysnail.js.emacs.en
-
-# # generate empty init file
-# ./generate-init-file.py ja empty > ./content/resources/.keysnail.js.ja
-# ./generate-init-file.py en empty > ./content/resources/.keysnail.js.en
-
-## ================================ ##
-
 # create jar file
 rm -f chrome/keysnail.jar
 jar cvf0 chrome/keysnail.jar \
@@ -38,3 +24,9 @@ zip -r -9 keysnail.xpi \
     components/*.js~*~
 cp chrome.manifest.pack /tmp/chrome.manifest
 zip -j -9 keysnail.xpi /tmp/chrome.manifest
+
+## ================================ ##
+
+# display hash for creating update info
+HASH=`shasum -a 1 keysnail.xpi | sed s'/[ ].*$//'`
+echo sha1:$HASH
