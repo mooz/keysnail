49c27374d3ad486457892375899f05572e453ecd
mooz <stillpedant@gmail.com>
2010-01-09 01:15:53 +0900

    Made hash to be automatically inserted into the update.rdf

--- createpackage.sh
+++ createpackage.sh
@@ -27,6 +27,9 @@ zip -j -9 keysnail.xpi /tmp/chrome.manifest
 
 ## ================================ ##
 
-# display hash for creating update info
+# copy hash for creating update info
 HASH=`shasum -a 1 keysnail.xpi | sed s'/[ ].*$//'`
-echo sha1:$HASH
+echo sha1:$HASH | xsel -ib
+mv update.rdf update.rdf.bak
+sed -e "s/em:updateHash=\".*\"/em:updateHash=\"sha1:$HASH\"/" update.rdf.bak > update.rdf
+
