2a3294df1522bfda69d6fb6cd76f74a56e8f5c70
mooz <stillpedant@gmail.com>
2010-10-27 22:42:07 +0900

    Yatck version 2.1.9. Bug fixes

--- plugins/yet-another-twitter-client-keysnail.ks.js
+++ plugins/yet-another-twitter-client-keysnail.ks.js
@@ -1317,8 +1317,8 @@ var twitterClient =
 
                              let name = collection[i];
 
-                             if (!util.confirm("Delete user from blacklist",
-                                               util.format("Are you sure to delete \"%s\" from blacklist?", name)))
+                             if (util.confirm("Delete user from blacklist",
+                                              util.format("Are you sure to delete \"%s\" from blacklist?", name)))
                              {
                                  collection.splice(i, 1);
                                  prompt.refresh();
@@ -3558,7 +3558,7 @@ var PLUGIN_INFO =
     <name>Yet Another Twitter Client KeySnail</name>
     <description>Make KeySnail behave like Twitter client</description>
     <description lang="ja">KeySnail を Twitter クライアントに</description>
-    <version>2.1.8</version>
+    <version>2.1.9</version>
     <updateURL>http://github.com/mooz/keysnail/raw/master/plugins/yet-another-twitter-client-keysnail.ks.js</updateURL>
     <iconURL>http://github.com/mooz/keysnail/raw/master/plugins/icon/yet-another-twitter-client-keysnail.icon.png</iconURL>
     <author mail="stillpedant@gmail.com" homepage="http://d.hatena.ne.jp/mooz/">mooz</author>
