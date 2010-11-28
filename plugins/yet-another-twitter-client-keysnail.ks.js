892e267921b05a70b4fe440fe051bb7ae7a12d19
mooz <stillpedant@gmail.com>
2010-11-28 21:58:40 +0900

    yatck: version 2.2.0 (fixed sudden suspension of the crawler)

--- plugins/yet-another-twitter-client-keysnail.ks.js
+++ plugins/yet-another-twitter-client-keysnail.ks.js
@@ -3567,7 +3567,7 @@ var PLUGIN_INFO =
     <name>Yet Another Twitter Client KeySnail</name>
     <description>Make KeySnail behave like Twitter client</description>
     <description lang="ja">KeySnail を Twitter クライアントに</description>
-    <version>2.1.9</version>
+    <version>2.2.0</version>
     <updateURL>http://github.com/mooz/keysnail/raw/master/plugins/yet-another-twitter-client-keysnail.ks.js</updateURL>
     <iconURL>http://github.com/mooz/keysnail/raw/master/plugins/icon/yet-another-twitter-client-keysnail.icon.png</iconURL>
     <author mail="stillpedant@gmail.com" homepage="http://d.hatena.ne.jp/mooz/">mooz</author>
