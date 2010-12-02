86c775195f573ca5a2be9dc43813b8db0753f6f4
mooz <stillpedant@gmail.com>
2010-12-03 01:58:27 +0900

    yatck: version 2.2.2 / use id_str instead of id (to support snowflake, new id-gen algorithm of Twitter)

--- plugins/yet-another-twitter-client-keysnail.ks.js
+++ plugins/yet-another-twitter-client-keysnail.ks.js
@@ -3569,7 +3569,7 @@ var PLUGIN_INFO =
     <name>Yet Another Twitter Client KeySnail</name>
     <description>Make KeySnail behave like Twitter client</description>
     <description lang="ja">KeySnail を Twitter クライアントに</description>
-    <version>2.2.1</version>
+    <version>2.2.2</version>
     <updateURL>http://github.com/mooz/keysnail/raw/master/plugins/yet-another-twitter-client-keysnail.ks.js</updateURL>
     <iconURL>http://github.com/mooz/keysnail/raw/master/plugins/icon/yet-another-twitter-client-keysnail.icon.png</iconURL>
     <author mail="stillpedant@gmail.com" homepage="http://d.hatena.ne.jp/mooz/">mooz</author>
