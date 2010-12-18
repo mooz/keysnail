5db0ee9e1205b2c9bd19a2a6c77f58a4d0fc2175
mooz <stillpedant@gmail.com>
2010-12-19 01:52:27 +0900

    use zip command instead of jar

--- createpackage.sh
+++ createpackage.sh
@@ -6,7 +6,7 @@ setopt extended_glob
 
 # create jar file
 rm -f chrome/keysnail.jar
-jar cvf0 chrome/keysnail.jar \
+zip -r -0 chrome/keysnail.jar \
     content/*.{js,xul,xhtml}~(*~|.svn/*) \
     content/resources/*~*~ \
     content/modules/*.js~(*~|.svn/*) \
