15971a1105613f05833814c7c4c438dd11d0992c
mooz <stillpedant@gmail.com>
2010-12-19 20:08:21 +0900

    make default interval 24 hour

--- defaults/preferences/keysnail-prefs.js
+++ defaults/preferences/keysnail-prefs.js
@@ -5,3 +5,4 @@ pref("extensions.keysnail.keyhandler.prevent_key_up_down", true);
 pref("extensions.keysnail.keyhandler.use_prefix_argument", false);
 pref("extensions.keysnail.vimp.enabled", true);
 pref("extensions.keysnail.plugin.run_update_checker_automatically", true);
+pref("extensions.keysnail.plugin.update_checker_interval", 24);
