de5d94400a3777eb676e071a9ff5f664fdc2ca4d
mooz <stillpedant@gmail.com>
2011-05-10 11:03:09 +0900

    bmany: Version 0.0.9

--- plugins/bmany.ks.js
+++ plugins/bmany.ks.js
@@ -3,7 +3,7 @@ var PLUGIN_INFO =
     <name>bmany</name>
     <description>Search bookmarks incrementally and go!</description>
     <description lang="ja">anything.el 気分でブックマークを操作</description>
-    <version>0.0.8</version>
+    <version>0.0.9</version>
     <updateURL>http://github.com/mooz/keysnail/raw/master/plugins/bmany.ks.js</updateURL>
     <iconURL>http://github.com/mooz/keysnail/raw/master/plugins/icon/bmany.icon.png</iconURL>
     <author mail="stillpedant@gmail.com" homepage="http://d.hatena.ne.jp/mooz/">mooz</author>
