11bd1a74ad13cbd90596efe8196a6bfa7540a9f0
mooz <stillpedant@gmail.com>
2011-06-22 22:35:29 +0900

    Add helper script which manipulates version information

---
+++ set_version_info.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require "readline"
+
+version     = Readline.readline("Input version> ", true)
+max_version = Readline.readline("Input max version> ", true)
+
+replace_info = {
+  "install.rdf" => {
+    /(<em:version>)(.*?)(<\/em:version>)/       => version,
+    /(<em:maxVersion>)(.*?)(<\/em:maxVersion>)/ => max_version,
+  },
+  "update.rdf"  => {
+    /(updateinfo\/)(.*?)(\.xhtml)/ => version,
+    /(em:version=")(.*?)(")/       => version,
+    /(em:maxVersion=")(.*?)(")/    => max_version
+  },
+}
+
+replace_info.each { |file_name, pairs|
+  content = open(file_name) { |file| file.read }
+  pairs.each { |pattern, replacer| content.gsub!(pattern, "\\1#{replacer}\\3") }
+  open(file_name, "w") { |file| file.write(content) }
+}
