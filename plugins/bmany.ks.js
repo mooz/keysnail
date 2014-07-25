5ee5977bf5b46d99bfca30dda0f5471985a039de
958 <5th958@gmail.com>
2014-07-25 20:15:23 +0900

    bmany: Support Firefox 31

--- plugins/bmany.ks.js
+++ plugins/bmany.ks.js
@@ -523,8 +523,11 @@ var bmany =
                  if (typeof getShortcutOrURI === "undefined") {
                      // Firefox 25 ~
                      Task.spawn(function () {
-                         let data = yield getShortcutOrURIAndPostData(aQuery);
-                         aCallback(data.url);
+                         let data = yield getShortcutOrURIAndPostData(aQuery,
+                             // Firefox 31 ~
+                             function (data) aCallback(data.url));
+                         // ~ Firefox 30
+                         if (data) aCallback(data.url);
                      });
                  } else {
                      // ~ Firefox 24
