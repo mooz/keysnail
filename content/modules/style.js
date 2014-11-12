4d038a36382b9ffaa412ce4b1d9bcdec1c5b0232
Masafumi Oyamada <stillpedant@gmail.com>
2014-11-13 00:17:16 +0900

    Revert "FIX #151 (TypeError caused by style.register)"

--- content/modules/style.js
+++ content/modules/style.js
@@ -83,21 +83,21 @@ const style = function () {
          *                style.XHTML);
          */
         register: function (aCss, aNs, aAgent) {
-            var arg = getArg(Array.from(arguments));
+            var arg = getArg(arguments);
 
             if (!sss.sheetRegistered.apply(sss, arg))
                 sss.loadAndRegisterSheet.apply(sss, arg);
         },
 
         unregister: function (aCss, aNs, aAgent) {
-            var arg = getArg(Array.from(arguments));
+            var arg = getArg(arguments);
 
             if (sss.sheetRegistered.apply(sss, arg))
                 sss.unregisterSheet.apply(sss, arg);
         },
 
         toggle: function (aCss, aNs, aAgent) {
-            var arg = getArg(Array.from(arguments));
+            var arg = getArg(arguments);
 
             if (sss.sheetRegistered.apply(sss, arg))
                 sss.unregisterSheet.apply(sss, arg);
