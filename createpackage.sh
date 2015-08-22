250f1160d6361ea35a30cccae9467eb55d4adae2
Masafumi Oyamada <stillpedant@gmail.com>
2015-08-22 14:57:50 +0900

    Separate xpi packaging script and computing package hash script

--- createpackage.sh
+++ createpackage.sh
@@ -46,9 +46,4 @@ zip -j -9 keysnail.xpi /tmp/chrome.manifest
 
 ## ================================ ##
 
-# copy hash for creating update info
-HASH=`shasum -a 1 keysnail.xpi | sed s'/[ ].*$//'`
-echo sha1:$HASH | xsel -ib
-mv update.rdf update.rdf.bak
-sed -e "s/em:updateHash=\".*\"/em:updateHash=\"sha1:$HASH\"/" update.rdf.bak > update.rdf
-
+./updatehash.sh
