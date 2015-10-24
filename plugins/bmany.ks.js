d4513d3e933ac79d6302e1b46e782dc53a650532
Masafumi Oyamada <stillpedant@gmail.com>
2015-10-24 22:48:04 +0900

    bmany: Version 0.1.3

--- plugins/bmany.ks.js
+++ plugins/bmany.ks.js
@@ -3,13 +3,13 @@ var PLUGIN_INFO =
     <name>bmany</name>
     <description>Search bookmarks incrementally and go!</description>
     <description lang="ja">anything.el 気分でブックマークを操作</description>
-    <version>0.1.2</version>
+    <version>0.1.3</version>
     <updateURL>http://github.com/mooz/keysnail/raw/master/plugins/bmany.ks.js</updateURL>
     <iconURL>http://github.com/mooz/keysnail/raw/master/plugins/icon/bmany.icon.png</iconURL>
     <author mail="stillpedant@gmail.com" homepage="http://d.hatena.ne.jp/mooz/">mooz</author>
     <license document="http://www.opensource.org/licenses/mit-license.php">The MIT License</license>
     <license lang="ja">MIT ライセンス</license>
-    <minVersion>1.5.4</minVersion>
+    <minVersion>34</minVersion>
     <include>main</include>
     <options>
         <option>
